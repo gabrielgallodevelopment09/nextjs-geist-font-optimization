8c6c5350cb3d58e0893c3f1dda0ff9ac88b4cf77
gabrielgallodevelopment09 <cpc3121@gmail.com>
2025-07-31 13:26:26 +0200

    Add .next/static/chunks/src_app_transactions_page_tsx_e03b4207._.js 1753961183231

---
+++ .next/static/chunks/src_app_transactions_page_tsx_e03b4207._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_828617fb._.js",
+  "static/chunks/node_modules_8aceda8d._.js"
+],
+    source: "dynamic"
+});
